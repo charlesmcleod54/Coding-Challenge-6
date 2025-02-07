f25ef365266a7c6bb2ec77484bee4b4bf42d509f
Charles McLeod <charlesmcleod@usf.edu>
2025-02-07 18:20:31 -0500

    Task 2 - Sales Tax Computation

--- cc_6s.js
+++ cc_6s.js
@@ -11,3 +11,15 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
 // Test data
 console.log(calculateProfit(20, 30, 100));
 console.log(calculateProfit(50, 70, 200));
+
+// Task 2
+// Declare function
+const calculateSalesTax = function(amount, taxRate) {
+    const salesTax = amount * taxRate;
+
+    return 'Sales Tax: $${salesTax}';
+};
+
+// Test data
+console.log(calculateSalesTax(100, 0.07));
+console.log(calculateSalesTax(500, 0.1));
